3eb80f19b345d11dffa9ee6d2fd07a5dd6e9ab9b
didier-bazayesu <didierbazayesu@gmail.com>
2026-02-13 10:37:58 +0200

    Initial commit: migrate Add Ingredients project from learning repository

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import HeaderIncredient from './components/HeaderIncredient'
+import ChangeBulb from './components/changeBulb'
+
+
+function App() {
+
+
+
+  return (
+    <div>
+       <ChangeBulb/>
+      <HeaderIncredient/>
+    </div>
+  )
+}
+
+export default App
